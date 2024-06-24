5a3c278e3864cd7151a975928989f7859e26157b
rahulbera9386 <rahulberastd2002@gmail.com>
2024-06-25 01:41:27 +0530

    Task 2 Completed

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const { fetchPosts } = require('./posts.service');
 const { fetchUserById } = require('../users/users.service');
+const axios = require('axios');
 
 const router = express.Router();
+
 
 router.get('/', async (req, res) => {
   const posts = await fetchPosts();
 
-  const postsWithImages = posts.reduce((acc, post) => {
-    // TODO use this route to fetch photos for each post
-    // axios.get(`https://jsonplaceholder.typicode.com/albums/${post.id}/photos`);
-    return [
-      ...acc,
-      {
-        ...post,
-        images: [
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/400' },
-          { url: 'https://picsum.photos/200/500' },
-        ],
-      },
-    ];
-  }, []);
+  const postsWithImages = await Promise.all(posts.map(async post => {
+    const { data: images } = await axios.get(`https://jsonplaceholder.typicode.com/albums/${post.id}/photos`);
+    return {
+      ...post,
+      images: images.map(image => ({ url: image.url })), 
+    };
+  }));
 
   res.json(postsWithImages);
 });
+
+
 
 module.exports = router;
